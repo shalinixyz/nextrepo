9cd6897ea8762edd81fdf1f34e73f67ed0bfb29a
Deepa <Deepa@Deepa-pc>
2018-03-30 20:03:34 +0530

    shalini commit

--- DemoProject/src/com/mccoy/firstApp/Democlass.java
+++ DemoProject/src/com/mccoy/firstApp/Democlass.java
@@ -5,7 +5,7 @@ public class Democlass {
 	public static void main(String[] args) {
 		
 		System.out.println("HEllo.......");
-		System.out.println("Hello manu");
+		System.out.println("Hello shalini");
 
 	}
 
